698551132e11944ba93945416cd4281ab0f429ca
twenfourl <rayapkun@gmail.com>
2023-09-03 04:29:58 +0000

    diff

--- anykernel.sh
+++ anykernel.sh
@@ -1,29 +1,44 @@
-### AnyKernel3 Ramdisk Mod Script
-## osm0sis @ xda-developers
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
 
-### AnyKernel setup
+## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=lilium kernul
+kernel.string= 
+maintainer.string= Kernel by ZHANtech™
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=tissot
-supported.versions=
+supported.versions=9 - 13.1
 supported.patchlevels=
 '; } # end properties
 
-## boot shell variables
+
+# shell variables
 block=/dev/block/platform/soc/7824900.sdhci/by-name/boot;
 is_slot_device=1;
 ramdisk_compression=auto;
 
-# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
-. tools/ak3-core.sh && attributes;
 
-# boot install
-dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
-write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
+
+## AnyKernel boot install
+dump_boot;
+# begin ramdisk changes
+
+write_boot;
 ## end boot install
+
+ui_print "kernel installed successfully"
